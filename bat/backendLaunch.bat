627c62b1f6940b023cf2b207fc4d9ff1c361616a
ratataque <ewanrimmely@outlook.fr>
2022-11-06 14:16:55 +0100

    réorga

--- bat/backendLaunch.bat
+++ bat/backendLaunch.bat
@@ -1,3 +1,2 @@
 cd ..
-docker build -t fraise .  
 docker run --rm -it -p 8000:8000 -v "%cd%\backend:/fraise/backend/" fraise
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './Components/Login/Login'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          dit <code>src/App.js</code> 
-        </p>
-        <a
-          className="App-lifnk"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ta mere la pute
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <Login />
+    )
+};
 
 export default App;
