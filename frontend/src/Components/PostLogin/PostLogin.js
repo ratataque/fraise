3b2e3705c57273774dbbb79fdc9cb09c71e02873
ratataque <ewanrimmely@outlook.fr>
2023-03-22 00:44:15 +0100

    big avancement postlogin

--- frontend/src/Components/PostLogin/PostLogin.js
+++ frontend/src/Components/PostLogin/PostLogin.js
@@ -1,8 +1,9 @@
-import React, {useEffect} from "react";
+import React, {useEffect, prevState} from "react";
 import "./PostLogin.css"
 import {Navbar} from "../../Components";
 import {useLocation } from 'react-router-dom';
-// import {TbWorld} from "react-icons/tb" //React-logo d'une fraise
+import {BsClipboardPlus} from "react-icons/bs" //React-logo d'une fraise
+import {AiOutlineEdit} from "react-icons/ai" //React-logo d'une fraise
 
 
 
@@ -13,7 +14,24 @@ function PostLogin() {
         // console.log(state)
     }, [])
 
+    const [switch_, toggle_switch] = React.useState("montre_display");
+    function marg_switch(elem) {
+        toggle_switch("")
+
+        setTimeout(() => {
+            toggle_switch(elem)
+        }, 300);
+    }
+
     const [add_password, toggle] = React.useState("");
+    const [edit_password, toggle_password] = React.useState(true);
+    const [edit_email, toggle_email] = React.useState(true);
+
+    var cacher = edit_password ? "cacher" : "";
+    var password = edit_password ? "password" : "text";
+    var email_change = !edit_email && edit_password ? "email_cacher" : "";
+    var active_email = !edit_email ? "active" : ""
+    var active_password = !edit_password ? "active" : ""
 
     return (
         <div className="postLogin">
@@ -60,13 +78,64 @@ function PostLogin() {
                     <div className="titre_website">Account list</div>
 
                     <div className="list_account_cont">
-                        <div className="list_account"></div>
+                        <div className="list_account">
+                            <div className="site underline" onClick={() => {toggle(""); marg_switch("montre_display"); toggle_email(true);}}>test@test.com</div>
+                            <div className="site underline" onClick={() => {toggle(""); marg_switch("montre_display"); toggle_email(true);}}>test@test.com</div>
+                            <div className="site underline" onClick={() => {toggle(""); marg_switch("montre_display"); toggle_email(true);}}>test@test.com</div>
+                        </div>
 
-                        <div className="encoche" onClick={() => toggle("add_password")}></div>
-                        <div className="addButton addButton--active" onClick={() => toggle("add_password")}></div>
+                        <div className="list_account_marg"></div>
+                        <div className="encoche" onClick={() => marg_switch()}></div>
+                        <div className="addButton addButton--active" onClick={() => {marg_switch("montre_add"); toggle_email(false); toggle_password(true)}}></div>
                         <div className="carre"></div>
                     </div>
                 </div>
+
+                <div className="display_password_cont">
+                    <div className="display_password_cont_titre ">
+                        <div className="titre_website">Site test</div>
+
+                        <div className={"marge " + switch_}>
+                            <div className="display_password">
+                                <div className="titre_email"> Email / Username :</div>
+                                <input type={"email"} className="email_field" readOnly={edit_email} value="test@test.com"></input>
+                                <button className={"btn_copy " + active_email} onClick={() => toggle_email(prevState=>!prevState)}><AiOutlineEdit/></button>
+                                <button className="btn_copy"><BsClipboardPlus/></button>
+
+                                <div className="titre_password">Password :</div>
+                                <input type={password} className="password_field" readOnly={edit_password} value="test"></input>
+                                <button className={"btn_copy " + active_password} onClick={() => toggle_password(prevState=>!prevState)}><AiOutlineEdit/></button>
+                                <button className="btn_copy"><BsClipboardPlus/></button>
+
+                                <div className={"titre_password " + cacher}>Confirm Password :</div>
+                                <input type={"text"} className={"password_field " + cacher} ></input>
+                                <button className={email_change + " btn_change " + cacher}>Change</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="new_password_cont">
+                        <div className="titre_website">Site test</div>
+
+                        <div className={"marge " + switch_}>
+                            <div className="display_password">
+                                <div className="titre_email"> Email / Username :</div>
+                                <input type={"email"} className="email_field" placeholder="Example@example.com" ></input>
+
+                                <div className="titre_password">Password :</div>
+                                <input type={password} className="password_field" readOnly={edit_password} value="test"></input>
+                                <button className={"btn_copy " + active_password} onClick={() => toggle_password(prevState=>!prevState)}><AiOutlineEdit/></button>
+
+                                <div className={"titre_password " + cacher}>Confirm Password :</div>
+                                <input type={"text"} className={"password_field " + cacher} ></input>
+                                <button className={email_change + " btn_change "}>Confirm</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     );
