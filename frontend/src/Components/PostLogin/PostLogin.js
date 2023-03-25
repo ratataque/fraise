be7bec9c60b4de661b7a97ab765334ce2b89a317
ratataque <ewanrimmely@outlook.fr>
2023-03-25 01:02:06 +0100

    du sale

--- frontend/src/Components/PostLogin/PostLogin.js
+++ frontend/src/Components/PostLogin/PostLogin.js
@@ -15,6 +15,32 @@ function PostLogin() {
     const [generated_password_change, change_generated_password] = useState("");
     const [generated_password_change_confirm, change_generated_password_confirm] = useState("");
 
+    const test_data = {
+                        "Site test 1": {"test1@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 2": {"test2@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 3": {"test3@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 4": {"test4@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 5": {"test5@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 6": {"test6@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 7": {"test7@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 8": {"test8@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 9": {"test9@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 10": {"test10@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 11": {"test11@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 12": {"test12@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 13": {"test13@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 14": {"test14@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                        "Site test 15": {"test15@test.com": "A9DC&d20H74ub1v99WF€?/Pwm717FtCypf81G1E116A7$"},
+                    }
+    const [website_dict, set_new_website_dict] = useState(test_data);
+    const [website_current, set_current_website] = useState("");
+
+    function set_current_website_func(e) {
+        setTimeout(() => {
+            set_current_website(e.target.getAttribute("id"))
+        }, 350);
+    }
+
     useEffect(() => {
         // console.log(state)
 
@@ -45,6 +71,18 @@ function PostLogin() {
             toggle_switch(elem)
         }, 300);
     }
+    const [transi, toggle_transi] = React.useState("");
+    function transi_website() {
+        if (add_password !== "add_password") {
+            toggle_transi("cacher")
+            toggle("")
+
+            setTimeout(() => {
+                toggle("transi")
+                toggle_transi("")
+            }, 350);
+        }
+    }
 
     const handle_email_change = (event) => {
         // change_generated_password(event.target.value)
@@ -56,9 +94,10 @@ function PostLogin() {
         change_generated_password_confirm(event.target.value)
     }
 
-    const [add_password, toggle] = React.useState("");
+
+    const [add_password, toggle] = React.useState("add_password");
     const [edit_password, toggle_password] = React.useState(true);
-    const [edit_email, toggle_email] = React.useState(true);
+    const [edit_email, toggle_email] = React.useState(false);
 
     var cacher = edit_password ? "cacher" : "";
     var password = edit_password ? "password" : "text";
@@ -79,36 +118,25 @@ function PostLogin() {
                 {/* <TbWorld color="www_icone white" size={36}/> */}
 
                 <div className="liste_website">
-                    <div className="encoche" onClick={() => toggle("add_password")}></div>
+                    <input className="recherche" placeholder="Recherche"></input>
+
+                    <div className="encoche" onClick={() => {toggle("add_password");toggle_email(false); toggle_password(true); setIsButtonClicked(!isButtonClicked)}}></div>
                     <div className="addButton addButton--active" onClick={() => {toggle("add_password");toggle_email(false); toggle_password(true); setIsButtonClicked(!isButtonClicked)}}></div>
                     <div className="carre"></div>
 
                     <div className="list_site_marg"></div>
                     <div className="list_site_cont">
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
-                        <div className="site underline" onClick={() => {toggle("");toggle_email(true); toggle_password(true);}}>Site test</div>
+                        {
+                            Object.entries(website_dict).map( ([key, value]) => <div id={key} className="site underline" onClick={(e) => {transi_website(); toggle("");toggle_email(true); toggle_password(true);set_current_website_func(e)}}>{key}</div>)
+                        }
                     </div>
                 </div>
             </div>
 
-            <div className={"add_password_container " + add_password} >
+            <div className={" add_password_container " + add_password} >
                     <div className="new_password_cont">
                         <div className="titre_website">Website : </div>
-                        <input id="email_add_new" type={"email"} className="website_field" placeholder="Example.com" ></input>
+                        <input type="text" className="website_field" placeholder="Example.com" ></input>
 
                         <div className={"marge " + switch_}>
                             <div className="display_password">
@@ -132,19 +160,22 @@ function PostLogin() {
                     </div>
             </div>
 
-            <div className={"password_container " + add_password}>
+            <div className={transi + " password_container " + add_password}>
                 <div className="account_container">
                     <div className="titre_website">Account list</div>
 
                     <div className="list_account_cont">
                         <div className="list_account">
+                        {
+                            website_current ? Object.entries(website_dict[website_current]).map( ([key, value]) => <div className="site underline" onClick={() => {toggle(""); marg_switch("montre_display"); toggle_email(true); toggle_password(true);}}>{key}</div>) : <div className="site underline" onClick={() => {marg_switch("montre_add"); toggle_email(false); toggle_password(true); setIsButtonClicked(!isButtonClicked);}}>Add</div>
+                        }
+                            {/* <div className="site underline" onClick={() => {toggle(""); marg_switch("montre_display"); toggle_email(true); toggle_password(true);}}>test@test.com</div>
                             <div className="site underline" onClick={() => {toggle(""); marg_switch("montre_display"); toggle_email(true); toggle_password(true);}}>test@test.com</div>
-                            <div className="site underline" onClick={() => {toggle(""); marg_switch("montre_display"); toggle_email(true); toggle_password(true);}}>test@test.com</div>
-                            <div className="site underline" onClick={() => {toggle(""); marg_switch("montre_display"); toggle_email(true); toggle_password(true);}}>test@test.com</div>
+                            <div className="site underline" onClick={() => {toggle(""); marg_switch("montre_display"); toggle_email(true); toggle_password(true);}}>test@test.com</div> */}
                         </div>
 
                         <div className="list_account_marg"></div>
-                        <div className="encoche" onClick={() => marg_switch()}></div>
+                        <div className="encoche" onClick={() => {marg_switch("montre_add"); toggle_email(false); toggle_password(true); setIsButtonClicked(!isButtonClicked);}}></div>
                         <div className="addButton addButton--active" onClick={() => {marg_switch("montre_add"); toggle_email(false); toggle_password(true); setIsButtonClicked(!isButtonClicked);}}></div>
                         <div className="carre"></div>
                     </div>
@@ -152,12 +183,12 @@ function PostLogin() {
 
                 <div className="display_password_cont">
                     <div className="display_password_cont_titre ">
-                        <div className="titre_website">Site test</div>
+                        <div className="titre_website">{website_current}</div>
 
                         <div className={"marge " + switch_}>
                             <div className="display_password">
                                 <div className="titre_email"> Email / Username :</div>
-                                <input id="email" type={"email"} className="email_field" readOnly={edit_email} value="test@test.com"></input>
+                                <input id="email" type={"email"} className="email_field" readOnly={edit_email} defaultValue="test@test.com"></input>
                                 <button className={"btn_copy " + active_email} onClick={() => toggle_email(!edit_email)}><AiOutlineEdit/></button>
                                 <button className="btn_copy" onClick={() => navigator.clipboard.writeText(document.getElementById("email").value)}><BsClipboardPlus/></button>
 
