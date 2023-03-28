f9f32c3a85f28000b4a1b2bb925fc824f13a97b7
estrakio <walterkarl@hotmail.fr>
2023-03-28 17:38:00 +0200

    compliqué la vie

--- frontend/src/Components/VerifEmail/VerifEmail.js
+++ frontend/src/Components/VerifEmail/VerifEmail.js
@@ -1,13 +1,29 @@
 import React from "react";
 import "./VerifEmail.css"
 import {Navbar} from "..";
+import ReactDOM from "react";
+import QRCode from "react-qr-code";
 // import {useLocation } from 'react-router-dom';
+
+ReactDOM.render(<QRCode value="hey" />, document.getElementById("Container"));
 
 function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
 }
+
+const token_otp = window.crypto.getRandomValues(new Uint32Array(2)).reduce(
+    (prev, curr, index) => (
+        prev = prev.toString(32).toUpperCase()
+    ) + (
+        curr =  curr.toString(32).toUpperCase()
+    )
+).split('').sort(() => 128 -
+    window.crypto.getRandomValues(new Uint32Array(1))[0]
+).join('')
+
+console.log(token_otp)
 
 
 function VerifEmail() {
@@ -40,7 +56,16 @@ function VerifEmail() {
             <Navbar/>
             {/* {verif(uuid)} */}
             <div id="verif_mail"> <p>{"Compte activé ! vous pouvez vous connecter."}</p></div>
+            <div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%"}}>
+            <QRCode
+            size={256}
+            style={{ height: "auto", maxWidth: "100%", width: "100%"}}
+            value={token_otp}
+            viewBox={'0 0 256 256'}
+            />
+            </div>
         </div>
+
     );
 }
 
